878f4f4a1676a41d0111a454add6b5bc1d2228d6
shankkalra <shankkalra@gmail.com>
2022-02-18 14:28:03 +0530

    Driver

--- src/test/java/bdd/MyStepDefinitions.java
+++ src/test/java/bdd/MyStepDefinitions.java
@@ -3,12 +3,19 @@ package bdd;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyStepDefinitions {
 
     @Given("I'm on the Store page")
     public void i_m_on_the_store_page() {
-
+        System.setProperty("webdriver.chrome.driver", "/home/shanky/Personal/Online Course/Testing/Driver_File/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://github.com/shank24");
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
     }
 
     @When("I add a {string} to the Cart")
