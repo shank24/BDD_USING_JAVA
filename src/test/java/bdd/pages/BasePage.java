8b6c467d92601f1a1ad511e11af6b98d9810db66
shankkalra <shankkalra@gmail.com>
2022-02-23 22:37:49 +0530

    Base Page

---
+++ src/test/java/bdd/pages/BasePage.java
@@ -0,0 +1,9 @@
+package bdd.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+}
