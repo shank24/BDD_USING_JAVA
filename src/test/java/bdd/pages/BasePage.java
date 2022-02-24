40c19520f173192ce44d767fa9e20cd9e6dc94f8
shankkalra <shankkalra@gmail.com>
2022-02-24 14:53:17 +0530

    Base Page

--- src/test/java/bdd/pages/BasePage.java
+++ src/test/java/bdd/pages/BasePage.java
@@ -1,9 +1,22 @@
 package bdd.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class BasePage {
-    private WebDriver driver;
-    private WebDriverWait wait;
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver,this);
+    }
+
+    public void load(String url){
+        driver.get(url);
+    }
 }
