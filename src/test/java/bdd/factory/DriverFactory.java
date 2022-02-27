25494d29810b42ef8b3c01537275b4abb98a1cf4
shankkalra <shankkalra@gmail.com>
2022-02-27 21:54:00 +0530

    Parallel Browser

--- src/test/java/bdd/factory/DriverFactory.java
+++ src/test/java/bdd/factory/DriverFactory.java
@@ -2,13 +2,26 @@ package bdd.factory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
     private static WebDriver driver;
 
-    public static WebDriver initializeDriver(){
-        System.setProperty("webdriver.chrome.driver", "/home/shanky/Personal/Online Course/Testing/Driver_File/chromedriver");
-        driver = new ChromeDriver();
+    public static WebDriver initializeDriver(String browser){
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "/home/shanky/Personal/Online Course/Testing/Driver_File/chromedriver");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "/home/shanky/Personal/Online Course/Testing/Driver_File/geckodriver");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                throw new IllegalStateException("INVALID BROWSER" + browser);
+        }
         driver.manage().window().maximize();
         return driver;
     }
